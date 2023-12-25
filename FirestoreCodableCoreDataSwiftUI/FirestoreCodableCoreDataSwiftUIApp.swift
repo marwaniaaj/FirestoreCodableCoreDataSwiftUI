3c749327beca7e13b8eac17efcef4a34bab7b956
Marwa <marwa.niaaj@gmail.com>
2023-12-25 13:46:52 +0400

    Initial Commit

---
+++ FirestoreCodableCoreDataSwiftUI/FirestoreCodableCoreDataSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirestoreCodableCoreDataSwiftUIApp.swift
+//  FirestoreCodableCoreDataSwiftUI
+//
+//  Created by Marwa Abou Niaaj on 25/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct FirestoreCodableCoreDataSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
