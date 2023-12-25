1fbb692ff6044a6b9733af8f3a1339bff3d94168
Marwa <marwa.niaaj@gmail.com>
2023-12-25 14:39:43 +0400

    Initialize and attach environment objects.

--- FirestoreCodableCoreDataSwiftUI/FirestoreCodableCoreDataSwiftUIApp.swift
+++ FirestoreCodableCoreDataSwiftUI/FirestoreCodableCoreDataSwiftUIApp.swift
@@ -5,13 +5,42 @@
 //  Created by Marwa Abou Niaaj on 25/12/2023.
 //
 
+import FirebaseCore
 import SwiftUI
 
 @main
 struct FirestoreCodableCoreDataSwiftUIApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+    @StateObject var dataController: DataController
+    @StateObject var firestoreManager: FirestoreManager
+
+    init() {
+        FirebaseApp.configure()
+        FirebaseConfiguration.shared.setLoggerLevel(.min)
+
+        let dataController = DataController()
+        _dataController = StateObject(wrappedValue: dataController)
+
+        let firestoreManager = FirestoreManager(dataController: dataController)
+        _firestoreManager = StateObject(wrappedValue: firestoreManager)
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, dataController.context)
+                .environmentObject(dataController)
+                .environmentObject(firestoreManager)
         }
     }
 }
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
